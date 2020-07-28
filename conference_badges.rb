f43dbb0b2ff25648d371596032eaa293bc83cea4
Luke Atencio <atencioluke@gmail.com>
2020-07-28 04:47:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names.each { |name| return "Hello, my name is #{name}."}
+  names.each do |name|
+    puts "Hello, my name is #{name}."
+  end
 end
