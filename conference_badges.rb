564d2cc6051316762607b4327425720497a04387
Luke Atencio <atencioluke@gmail.com>
2020-07-28 04:52:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,7 @@ end
 def batch_badge_creator(names)
   names.map { |name| "Hello, my name is #{name}." }
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index { |attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index + 1}"}
+end
